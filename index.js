e87d2c42aad636f683b7ddfd3072829615c89aef
Oskar Siry <oskarsiry99@gmail.com>
2024-09-12 18:13:30 +0200

    script for carosusel created

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cars = document.querySelectorAll('.carsGallery');
 const textButtons = document.querySelectorAll('.expandButton');
 const expandTexts = document.querySelectorAll('.expandText');
 
+const carouselButtons = document.querySelectorAll('.carouselButton');
+const carouselContainer = document.querySelector('.carouselContainer');
+let previousIndex = 0;
+
 function toggleContainers(indexToShow) {
   cars.forEach((car, index) => {
     index === indexToShow
@@ -22,7 +26,7 @@ buttons.forEach((button, index) => {
 
 function toggleExpandTexts(indexToExpand) {
   expandTexts.forEach((expandText, index) => {
-    index === indexToExpand ? expandText.classList.toggle('h-fit') : null;
+    index === indexToExpand ? expandText.classList.toggle('h-[42px]') : null;
   });
 }
 
@@ -35,5 +39,25 @@ textButtons.forEach((textButton, index) => {
       ? (spanElement.innerHTML = 'Zwiń')
       : (spanElement.innerHTML = 'Rozwiń');
     toggleExpandTexts(index);
+  });
+});
+
+carouselButtons.forEach((carouselButton, index) => {
+  carouselButton.addEventListener('click', () => {
+    document.querySelector('.activeDot')?.classList.remove('activeDot');
+    carouselButton.classList.add('activeDot');
+    const containerWidth = carouselContainer.clientWidth;
+    console.log(previousIndex);
+    cars.forEach((car) => {
+      if (index > previousIndex) {
+        car.scrollLeft += containerWidth;
+      } else if (index < previousIndex) {
+        car.scrollLeft -= containerWidth;
+      } else {
+        none;
+      }
+    });
+    previousIndex = index;
+    console.log(previousIndex);
   });
 });
